3bb69e22c1fb6ac7fe4d04bdae39ea8d26994482
Risad Hossain Home <mmdrislam@gmail.com>
2022-10-08 03:01:50 +0600

    text theme

--- lib/presentaion/theme_manager.dart
+++ lib/presentaion/theme_manager.dart
@@ -6,51 +6,60 @@ import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-    //Main color of the app
-    primaryColor: Colormanager.primary,
-    primaryColorLight: Colormanager.primaryOpacity70,
-    primaryColorDark: Colormanager.darkPrimary,
-    disabledColor: Colormanager.grey1,
-    //ripple color
-    splashColor: Colormanager.primaryOpacity70,
-    colorScheme:
-        ColorScheme.fromSwatch().copyWith(secondary: Colormanager.grey),
+      //Main color of the app
+      primaryColor: Colormanager.primary,
+      primaryColorLight: Colormanager.primaryOpacity70,
+      primaryColorDark: Colormanager.darkPrimary,
+      disabledColor: Colormanager.grey1,
+      //ripple color
+      splashColor: Colormanager.primaryOpacity70,
+      colorScheme:
+          ColorScheme.fromSwatch().copyWith(secondary: Colormanager.grey),
 
-    //card view theme
-    cardTheme: CardTheme(
-        color: Colormanager.white,
-        shadowColor: Colormanager.grey,
-        elevation: 0),
+      //card view theme
+      cardTheme: CardTheme(
+          color: Colormanager.white,
+          shadowColor: Colormanager.grey,
+          elevation: 0),
 
-    //App ba theme
-    appBarTheme: AppBarTheme(
-        centerTitle: true,
-        color: Colormanager.primary,
-        elevation: AppSize.s4,
-        shadowColor: Colormanager.primaryOpacity70,
-        titleTextStyle:
-            getRegularStyle(color: Colormanager.white, fontSize: FontSize.s16)),
+      //App ba theme
+      appBarTheme: AppBarTheme(
+          centerTitle: true,
+          color: Colormanager.primary,
+          elevation: AppSize.s4,
+          shadowColor: Colormanager.primaryOpacity70,
+          titleTextStyle: getRegularStyle(
+              color: Colormanager.white, fontSize: FontSize.s16)),
 
-    //Button theme
-    buttonTheme: ButtonThemeData(
-        shape: const StadiumBorder(),
-        disabledColor: Colormanager.grey1,
-        buttonColor: Colormanager.primary,
-        splashColor: Colormanager.primaryOpacity70),
+      //Button theme
+      buttonTheme: ButtonThemeData(
+          shape: const StadiumBorder(),
+          disabledColor: Colormanager.grey1,
+          buttonColor: Colormanager.primary,
+          splashColor: Colormanager.primaryOpacity70),
 
-    //Elevated button theme
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        textStyle: getRegularStyle(color: Colormanager.white),
-        backgroundColor: Colormanager.primary,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(AppSize.s12),
+      //Elevated button theme
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          textStyle: getRegularStyle(color: Colormanager.white),
+          backgroundColor: Colormanager.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(AppSize.s12),
+          ),
         ),
       ),
-    ),
 
-    //Text theme
+      //Text theme
+      textTheme: TextTheme(
+          headline1: getSemiBoldStyle(
+              color: Colormanager.darkGrey, fontSize: FontSize.s16),
+          subtitle1: getMediumStyle(
+            color: Colormanager.lightGrey,
+            fontSize: FontSize.s14,
+          ),
+          caption: getRegularStyle(color: Colormanager.grey1),
+          bodyText1: getRegularStyle(color: Colormanager.grey))
 
-    //input decoration theme(text form field)
-  );
+      //input decoration theme(text form field)
+      );
 }
